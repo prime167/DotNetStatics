969aca47d11779104e7dae15f2f3b241e1925b6f
prime167 <3722845+prime167@users.noreply.github.com>
2022-07-14 16:03:54 +0800

    small image

--- DotNetStatics/Form1.Designer.cs
+++ DotNetStatics/Form1.Designer.cs
@@ -55,7 +55,7 @@
             this.formsPlot1.Location = new System.Drawing.Point(34, 251);
             this.formsPlot1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.formsPlot1.Name = "formsPlot1";
-            this.formsPlot1.Size = new System.Drawing.Size(839, 392);
+            this.formsPlot1.Size = new System.Drawing.Size(673, 337);
             this.formsPlot1.TabIndex = 0;
             // 
             // BtnPolynomialRegression
@@ -64,7 +64,7 @@
             this.BtnPolynomialRegression.Name = "BtnPolynomialRegression";
             this.BtnPolynomialRegression.Size = new System.Drawing.Size(113, 31);
             this.BtnPolynomialRegression.TabIndex = 1;
-            this.BtnPolynomialRegression.Text = "Polynomial regression";
+            this.BtnPolynomialRegression.Text = "Polynomial";
             this.BtnPolynomialRegression.UseVisualStyleBackColor = true;
             this.BtnPolynomialRegression.Click += new System.EventHandler(this.BtnPolynomialRegression_Click);
             // 
@@ -91,7 +91,7 @@
             // LblMaxDiff
             // 
             this.LblMaxDiff.AutoSize = true;
-            this.LblMaxDiff.Location = new System.Drawing.Point(256, 141);
+            this.LblMaxDiff.Location = new System.Drawing.Point(192, 127);
             this.LblMaxDiff.Name = "LblMaxDiff";
             this.LblMaxDiff.Size = new System.Drawing.Size(33, 17);
             this.LblMaxDiff.TabIndex = 4;
@@ -99,7 +99,7 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(143, 93);
+            this.trackBar1.Location = new System.Drawing.Point(79, 79);
             this.trackBar1.Maximum = 100;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(235, 45);
@@ -112,7 +112,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(97, 107);
+            this.label1.Location = new System.Drawing.Point(66, 127);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(26, 17);
             this.label1.TabIndex = 6;
@@ -121,7 +121,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(396, 107);
+            this.label2.Location = new System.Drawing.Point(298, 127);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(40, 17);
             this.label2.TabIndex = 7;
@@ -166,7 +166,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(295, 141);
+            this.label4.Location = new System.Drawing.Point(231, 127);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(28, 17);
             this.label4.TabIndex = 12;
@@ -175,7 +175,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(700, 141);
+            this.label6.Location = new System.Drawing.Point(618, 127);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(51, 17);
             this.label6.TabIndex = 17;
@@ -184,16 +184,16 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(801, 107);
+            this.label7.Location = new System.Drawing.Point(685, 127);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(22, 17);
+            this.label7.Size = new System.Drawing.Size(15, 17);
             this.label7.TabIndex = 16;
-            this.label7.Text = "10";
+            this.label7.Text = "6";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(502, 107);
+            this.label8.Location = new System.Drawing.Point(472, 118);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(15, 17);
             this.label8.TabIndex = 15;
@@ -202,19 +202,20 @@
             // trackBar2
             // 
             this.trackBar2.LargeChange = 1;
-            this.trackBar2.Location = new System.Drawing.Point(548, 93);
+            this.trackBar2.Location = new System.Drawing.Point(472, 79);
+            this.trackBar2.Maximum = 6;
             this.trackBar2.Minimum = 1;
             this.trackBar2.Name = "trackBar2";
             this.trackBar2.Size = new System.Drawing.Size(235, 45);
             this.trackBar2.TabIndex = 14;
             this.trackBar2.TickFrequency = 10;
-            this.trackBar2.Value = 5;
+            this.trackBar2.Value = 2;
             this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
             // 
             // LblMaxDegree
             // 
             this.LblMaxDegree.AutoSize = true;
-            this.LblMaxDegree.Location = new System.Drawing.Point(661, 141);
+            this.LblMaxDegree.Location = new System.Drawing.Point(585, 127);
             this.LblMaxDegree.Name = "LblMaxDegree";
             this.LblMaxDegree.Size = new System.Drawing.Size(15, 17);
             this.LblMaxDegree.TabIndex = 13;
@@ -224,7 +225,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(921, 690);
+            this.ClientSize = new System.Drawing.Size(760, 600);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label8);
