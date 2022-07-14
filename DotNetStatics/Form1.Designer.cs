41e5c67fa2428bf6cb97502b367f124d97584907
prime167 <3722845+prime167@users.noreply.github.com>
2022-07-14 14:34:24 +0800

    .

--- DotNetStatics/Form1.Designer.cs
+++ DotNetStatics/Form1.Designer.cs
@@ -36,15 +36,26 @@
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LblReal = new System.Windows.Forms.Label();
+            this.LblFitting = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.LblMaxDegree = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             this.SuspendLayout();
             // 
             // formsPlot1
             // 
-            this.formsPlot1.Location = new System.Drawing.Point(34, 79);
+            this.formsPlot1.Location = new System.Drawing.Point(34, 251);
             this.formsPlot1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.formsPlot1.Name = "formsPlot1";
-            this.formsPlot1.Size = new System.Drawing.Size(754, 392);
+            this.formsPlot1.Size = new System.Drawing.Size(839, 392);
             this.formsPlot1.TabIndex = 0;
             // 
             // BtnPolynomialRegression
@@ -63,7 +74,7 @@
             this.BtnLineFitting.Name = "BtnLineFitting";
             this.BtnLineFitting.Size = new System.Drawing.Size(113, 31);
             this.BtnLineFitting.TabIndex = 2;
-            this.BtnLineFitting.Text = "Line Fitting";
+            this.BtnLineFitting.Text = "Linear";
             this.BtnLineFitting.UseVisualStyleBackColor = true;
             this.BtnLineFitting.Click += new System.EventHandler(this.BtnLineFitting_Click);
             // 
@@ -80,7 +91,7 @@
             // LblMaxDiff
             // 
             this.LblMaxDiff.AutoSize = true;
-            this.LblMaxDiff.Location = new System.Drawing.Point(638, 60);
+            this.LblMaxDiff.Location = new System.Drawing.Point(256, 141);
             this.LblMaxDiff.Name = "LblMaxDiff";
             this.LblMaxDiff.Size = new System.Drawing.Size(33, 17);
             this.LblMaxDiff.TabIndex = 4;
@@ -88,7 +99,7 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(525, 12);
+            this.trackBar1.Location = new System.Drawing.Point(143, 93);
             this.trackBar1.Maximum = 100;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(235, 45);
@@ -101,7 +112,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(479, 26);
+            this.label1.Location = new System.Drawing.Point(97, 107);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(26, 17);
             this.label1.TabIndex = 6;
@@ -110,17 +121,120 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(778, 26);
+            this.label2.Location = new System.Drawing.Point(396, 107);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(40, 17);
             this.label2.TabIndex = 7;
             this.label2.Text = "100%";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Real:";
+            // 
+            // LblReal
+            // 
+            this.LblReal.AutoSize = true;
+            this.LblReal.Location = new System.Drawing.Point(104, 173);
+            this.LblReal.Name = "LblReal";
+            this.LblReal.Size = new System.Drawing.Size(38, 17);
+            this.LblReal.TabIndex = 9;
+            this.LblReal.Text = "ax+b";
+            // 
+            // LblFitting
+            // 
+            this.LblFitting.AutoSize = true;
+            this.LblFitting.Location = new System.Drawing.Point(104, 212);
+            this.LblFitting.Name = "LblFitting";
+            this.LblFitting.Size = new System.Drawing.Size(38, 17);
+            this.LblFitting.TabIndex = 11;
+            this.LblFitting.Text = "ax+b";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Fitting:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(295, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Diff";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(700, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Degree";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(801, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "10";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(502, 107);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "1";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 1;
+            this.trackBar2.Location = new System.Drawing.Point(548, 93);
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(235, 45);
+            this.trackBar2.TabIndex = 14;
+            this.trackBar2.TickFrequency = 10;
+            this.trackBar2.Value = 5;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // LblMaxDegree
+            // 
+            this.LblMaxDegree.AutoSize = true;
+            this.LblMaxDegree.Location = new System.Drawing.Point(661, 141);
+            this.LblMaxDegree.Name = "LblMaxDegree";
+            this.LblMaxDegree.Size = new System.Drawing.Size(15, 17);
+            this.LblMaxDegree.TabIndex = 13;
+            this.LblMaxDegree.Text = "2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(830, 506);
+            this.ClientSize = new System.Drawing.Size(921, 690);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.LblMaxDegree);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LblFitting);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LblReal);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.trackBar1);
@@ -130,9 +244,10 @@
             this.Controls.Add(this.BtnPolynomialRegression);
             this.Controls.Add(this.formsPlot1);
             this.Name = "Form1";
-            this.Text = "Line Fitting";
+            this.Text = "Regression";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,5 +263,15 @@
         private TrackBar trackBar1;
         private Label label1;
         private Label label2;
+        private Label label3;
+        private Label LblReal;
+        private Label LblFitting;
+        private Label label5;
+        private Label label4;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TrackBar trackBar2;
+        private Label LblMaxDegree;
     }
 }
--- DotNetStatics/Form1.cs
+++ DotNetStatics/Form1.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using MathNet.Numerics;
 using ScottPlot;
 
@@ -12,6 +13,7 @@ namespace DotNetStatics
         private double[] _ys1;
         private int _maxDiff;
         private int _lastType;
+        private int _maxDegree;
 
         private Random _random;
 
@@ -27,31 +29,43 @@ namespace DotNetStatics
             formsPlot1.Plot.XLabel("x");
             formsPlot1.Plot.Title("");
             formsPlot1.Refresh();
+            trackBar1.Value = 10;
+            trackBar1.Value = 12;
+            _maxDiff = trackBar1.Value;
+            _maxDegree = trackBar2.Value;
         }
 
         private void BtnLineFitting_Click(object sender, EventArgs e)
         {
             RestPlot();
+            trackBar2.Enabled = false;
             _count = 100;
             _xs = new double[_count];
             _ys = new double[_count];
 
             _xs1 = new double[_count];
             _ys1 = new double[_count];
+            var a = _random.Next(1, 10);
+            var b = _random.Next(1, 10);
+            LblReal.Text = $"{a} * x + {b}";
             for (int i = 0; i < _count; i++)
             {
                 var md = _random.Next(-_maxDiff, _maxDiff) / 100.0;
                 _xs[i] = i;
-                _ys[i] = i;
+                _ys[i] = a * i + b;
                 _ys[i] = _ys[i] *= (1 + md);
             }
 
-            (double a, double b) = Fit.Line(_xs, _ys);
+            (double b1, double a1) = Fit.Line(_xs, _ys);
             for (int i = 0; i < _count; i++)
             {
                 _xs1[i] = i;
-                _ys1[i] = a + b * i;
+                _ys1[i] = a1 * i + b1;
             }
+
+            a1 = Math.Round(a1, 2);
+            b1 = Math.Round(b1, 2);
+            LblFitting.Text = $"{a1} * x + {b1}";
 
             Plot();
             _lastType = 1;
@@ -60,29 +74,48 @@ namespace DotNetStatics
         private void BtnPolynomialRegression_Click(object sender, EventArgs e)
         {
             RestPlot();
+            trackBar2.Enabled = true;
             _count = 100;
-            var maxPower = 2;
             _xs = new double[_count];
             _ys = new double[_count];
 
             _xs1 = new double[_count];
             _ys1 = new double[_count];
+
+            var co = new List<double>(10);
+            for (int i = 0; i <= _maxDegree; i++)
+            {
+                var r = _random.Next(0, 10);
+                if (i == 0 && r == 0)
+                {
+                    r = _maxDegree;
+                }
+
+                co.Add(r);
+            }
+
+            LblReal.Text = BuildPolynomial(_maxDegree, co);
+
             for (int i = 0; i < _count; i++)
             {
                 var md = _random.Next(-_maxDiff, _maxDiff) / 100.0;
                 _xs[i] = i;
-                double y = 0.1 * i;
-                _ys[i] = 6 * y * y * y + 2 * y * y + 7 * y;
+                double y = i;
+                for (int j = 0; j <= _maxDegree; j++)
+                {
+                    _ys[i] += co[j] * Math.Pow(y, _maxDegree - j);
+                }
+
                 _ys[i] *= (1 + md);
             }
 
-            double[] w = Fit.Polynomial(_xs, _ys, maxPower);
+            int order = _maxDegree;
+            double[] w = Fit.Polynomial(_xs, _ys, order);
             for (int i = 0; i < _count; i++)
             {
-
                 _xs1[i] = i;
                 var sum = 0.0;
-                for (int p = maxPower; p >= 0; p--)
+                for (int p = _maxDegree; p >= 0; p--)
                 {
                     sum += w[p] * Math.Pow(i, p);
                 }
@@ -90,8 +123,24 @@ namespace DotNetStatics
                 _ys1[i] = sum;
             }
 
+            LblFitting.Text = BuildPolynomial(order, w.Reverse());
             Plot();
             _lastType = 2;
+        }
+
+        private string BuildPolynomial(int maxPower, IEnumerable<double> co)
+        {
+            var list = co.ToList();
+            var sb = new StringBuilder();
+            for (int i = 0; i < maxPower; i++)
+            {
+                list[i] = Math.Round(list[i], 2);
+                sb.Append($"{list[i]} * x^{maxPower - i} + ");
+            }
+
+            var q = Math.Round(list[^1], 2);
+            sb.Append($"{q}");
+            return sb.ToString().Replace("x^1","x").Replace("+ -","- ");
         }
 
         private void BtnReset_Click(object sender, EventArgs e)
@@ -109,7 +158,7 @@ namespace DotNetStatics
         {
             formsPlot1.Plot.Legend(true, Alignment.UpperLeft);
             var plot = formsPlot1.Plot.AddScatter(_xs, _ys, Color.Blue, 1, 5, MarkerShape.cross, LineStyle.None);
-            plot.Label = "data";
+            plot.Label = "orginal data";
 
             var plot1 = formsPlot1.Plot.AddScatterLines(_xs1, _ys1, Color.Red);
             plot1.Label = "fitting";
@@ -126,6 +175,20 @@ namespace DotNetStatics
                 BtnLineFitting_Click(this, EventArgs.Empty);
             }
             else if (_lastType == 2)
+            {
+                BtnPolynomialRegression_Click(this, EventArgs.Empty);
+            }
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            _maxDegree = trackBar2.Value;
+            LblMaxDegree.Text = _maxDegree.ToString();
+            if (_lastType == 1)
+            {
+                BtnLineFitting_Click(this, EventArgs.Empty);
+            }
+            else if (_lastType == 2 || _lastType == 0)
             {
                 BtnPolynomialRegression_Click(this, EventArgs.Empty);
             }
